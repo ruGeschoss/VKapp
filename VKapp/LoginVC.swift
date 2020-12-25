8f12bcbb69e3d9c144e7c7989630a9f3cd9013fc
Alex <tracktouch@gmail.com>
2020-12-25 18:58:29 +0300

    lessonTwoUI
    Navigation, Tab view

--- VKapp/LoginVC.swift
+++ VKapp/LoginVC.swift
@@ -8,9 +8,13 @@
 import UIKit
 
 class LoginVC: UIViewController {
+//    MARK: - Outlets
+    
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var loginTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
+    
+//    MARK: - Functions
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,9 +25,8 @@ class LoginVC: UIViewController {
         
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow (notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide (notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
-        
-        // Do any additional setup after loading the view.
     }
+    
     @objc func keyboardWillShow (notification: Notification) {
         guard let kbSize = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect else { return }
         let insets = UIEdgeInsets(top: 0, left: 0, bottom: kbSize.size.height, right: 0)
@@ -34,13 +37,44 @@ class LoginVC: UIViewController {
         let insets = UIEdgeInsets.zero
         scrollView.contentInset = insets
     }
+    
     @objc func didTapOnScroll () {
         view.endEditing(true)
     }
     
-    @IBAction func SignInButton(_ sender: UIButton) {
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        switch identifier {
+        case "login_success":
+            let checkResult = checkUserData()
+            if !checkResult {
+                showLoginError()
+            }
+            return checkResult
+        default:
+            return true
+        }
     }
-
+    
+    func checkUserData () -> Bool {
+        guard let login = loginTextField.text, let password = passwordTextField.text else { return false }
+        if login == "admin" && password == "admin" {
+            return true
+        } else {
+            return false
+        }
+    }
+    func showLoginError() {
+        let alert = UIAlertController(title: "Ошибка", message: "Введите admin в оба поля", preferredStyle: .alert)
+        let action = UIAlertAction(title: "ОК", style: .cancel, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+//    MARK: - Actions
+    
+    @IBAction func myUnwindAction(unwindSegue: UIStoryboardSegue) {}
+    @IBAction func signInButton(_ sender: UIButton) {}
+    @IBAction func signUpButton(_ sender: UIButton) {}
     /*
     // MARK: - Navigation
 
@@ -50,6 +84,5 @@ class LoginVC: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
     
 }
---
+++ VKapp/RegistrationVC.swift
@@ -0,0 +1,15 @@
+//
+//  RegistrationVC.swift
+//  VKapp
+//
+//  Created by Александр Андрианов on 24.12.2020.
+//
+
+import UIKit
+
+class RegistrationVC: LoginVC {
+        
+    override func viewDidLoad() {
+    }
+    
+}
